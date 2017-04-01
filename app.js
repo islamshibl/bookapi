26d1b9edad63feaf0988d028412fc04a520e8dbd
islamshibl <islam-shibl@hotmail.com>
2017-04-01 09:20:02 +0200

    Adding gulp

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var app  =express();
 var port =process.env.port ||3000;
 
 app.get('/',function(req,res){
-res.send('welcome to my API');
+res.send('Hello, welcome to my API ');
 });
 
 app.listen(port,function(){
-    console.log('Running on PORT '+port);
+    console.log('Gulp is Running my app on PORT '+port);
 });
