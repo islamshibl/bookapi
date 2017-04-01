cc0d5ab034a977be7f1f7c1ab679374875156036
islamshibl <islam-shibl@hotmail.com>
2017-04-01 10:09:10 +0200

    Adding mongos creating model getting data from monbodb

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 var express =require('express');
+    mongoose =require('mongoose');
 
 
+var db =mongoose.connect('mongodb://localhost/bookAPI');
+var Book =require('./models/bookModel');
 var app  =express();
 
 var port =process.env.port ||3000;
+
+//add routes
+
+var bookRouter =express.Router();
+
+bookRouter.route('/Books')
+.get(function(req, res){
+    var query ={};
+    if(req.query.genre){
+        query.genre =req.query.genre;
+    }
+    Book.find(query,function(err,books){
+        if(err)
+           res.status(500).send(err);
+        else
+            res.json(books);
+    });
+   // res.json(responseJson);
+});
+
+app.use('/api',bookRouter)
+
 
 app.get('/',function(req,res){
 res.send('Hello, welcome to my API ');
@@ -12,3 +37,4 @@ res.send('Hello, welcome to my API ');
 app.listen(port,function(){
     console.log('Gulp is Running my app on PORT '+port);
 });
+
