079a5cb6b5f349fc3dd1fcc43b7982e60a9c8178
islamshibl <islam-shibl@hotmail.com>
2017-04-07 11:23:57 +0200

    Added middleware code and patch request.

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -1,58 +1,87 @@
 var express = require('express');
 
-var routes =function(Book){
-var bookRouter =express.Router();
+var routes = function (Book) {
+    var bookRouter = express.Router();
 
-bookRouter.route('/')
-.post(function(req,res){
-    var book = new Book(req.body);
-    book.save();
-    res.status(201).send(book);
-    
-})
-.get(function(req, res){
-    var query ={};
-    if(req.query.genre){
-        query.genre =req.query.genre;
-    }
-    Book.find(query,function(err,books){
-        if(err){
-           res.status(500).send(err);}
-        else{
-            res.json(books);
-            console.log("data")
-    }
-    });
-   // res.json(responseJson);
-});
-
-bookRouter.route('/:bookId')
-.get(function(req, res){
-  
-   
-    Book.findById(req.params.bookId,function(err,books){
-        if(err)
-           res.status(500).send(err);
-        else
-            res.json(books);
-    });
-   // res.json(responseJson);
-})
-.put(function(req,res){
- Book.findById(req.params.bookId,function(err,book){
-        if(err)
-           res.status(500).send(err);
-        else            
-            book.ttle= req.body.title;
-            book.author=req.body.author;
-            book.genre=req.body.genre;
-            book.read=req.body.read;
+    bookRouter.route('/')
+        .post(function (req, res) {
+            var book = new Book(req.body);
             book.save();
-            res.json(book);
-    });
-})
-;
-return bookRouter;
+            res.status(201).send(book);
+
+        })
+        .get(function (req, res) {
+            var query = {};
+            if (req.query.genre) {
+                query.genre = req.query.genre;
+            }
+            Book.find(query, function (err, books) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.json(books);
+                    console.log("data")
+                }
+            });
+            // res.json(responseJson);
+        });
+    //middleware code....
+    bookRouter.use('/:bookId', function (req, res, next) {
+
+        Book.findById(req.params.bookId, function (err, book) {
+            if (err)
+                res.status(500).send(err);
+            else if (book) {
+                req.book = book;
+                next();
+            }
+            else {
+                res.status(404).send('no book found');
+            }
+
+        });
+    })
+
+    bookRouter.route('/:bookId')
+        .get(function (req, res) {
+            res.json(req.body);
+        })
+        .put(function (req, res) {
+            Book.findById(req.params.bookId, function (err, book) {
+
+
+                req.book.ttle = req.body.title;
+                req.book.author = req.body.author;
+                req.book.genre = req.body.genre;
+                req.book.read = req.body.read;
+                req.book.save(function(err){
+                  if (err)
+                res.status(500).send(err);
+                else{
+                   res.json(req.book); 
+                }
+            });
+                //res.json(req.book);
+
+            });
+        })
+        .patch(function (req, res) {
+            if (req.body._id)
+                delete req.body._id;
+            for (var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+            req.book.save(function(err){
+                  if (err)
+                res.status(500).send(err);
+                else{
+                   res.json(req.book); 
+                }
+            });
+        })
+        ;
+    return bookRouter;
 };
 
 module.exports = routes;
